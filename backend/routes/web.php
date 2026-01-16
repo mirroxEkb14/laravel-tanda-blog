de8baef0132a1bc2b588583de96a6164865c532b
Daniyar <dancho.rancho07@gmail.com>
2026-01-16 03:17:26 +0100

    Add Scramble Swagger routes

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use Dedoc\Scramble\Scramble;
 use Illuminate\Support\Facades\Route;
+
+Scramble::registerUiRoutes('docs/swagger');
+Scramble::registerJsonRoutes('docs/swagger.json');
 
 Route::get('/', function () {
     return view('welcome');
