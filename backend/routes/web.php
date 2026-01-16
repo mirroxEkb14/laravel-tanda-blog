a0b2bc7682f63e8532c4f3d19dc793fbada2db24
Daniyar <dancho.rancho07@gmail.com>
2026-01-16 03:26:27 +0100

    Fix Scramble route registration

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -3,8 +3,8 @@
 use Dedoc\Scramble\Scramble;
 use Illuminate\Support\Facades\Route;
 
-Scramble::registerUiRoutes('docs/swagger');
-Scramble::registerJsonRoutes('docs/swagger.json');
+Scramble::registerUiRoute('docs/swagger');
+Scramble::registerJsonRoute('docs/swagger.json');
 
 Route::get('/', function () {
     return view('welcome');
